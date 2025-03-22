fd2175c1f54fe36b27119b5d4f8d443fef676bfd
Berke Can Ünlü <berke.c.unlu@gmail.com>
2025-03-22 22:53:43 +0300

    phase-1 done

--- code/term-project-python-processor/main.py
+++ code/term-project-python-processor/main.py
@@ -3,15 +3,19 @@ from nats.aio.client import Client as NATS
 import os, random
 from scapy.all import Ether
 from nats.aio.client import Msg
+import queue
+import argparse
 
 class Processor:
-    def __init__(self):
+    def __init__(self, delay_mean: float):
         self.nc = NATS()
         self.nats_url = os.getenv("NATS_SURVEYOR_SERVERS", "nats://nats:4222")
         self.INPKT_SEC = "inpktsec"
         self.INPKT_INSEC = "inpktinsec"
         self.OUTPKT_SEC = "outpktsec" 
         self.OUTPKT_INSEC = "outpktinsec"
+        self.delay_mean = delay_mean
+        self.delays = queue.Queue(maxsize=1000)
 
     async def connect(self):
         await self.nc.connect(self.nats_url)
@@ -32,7 +36,7 @@ class Processor:
         await self.nc.subscribe(topic, cb=self.message_handler)
 
     async def publish(self, topic: str, data: bytes):
-        delay = self.create_random_delay()
+        delay = self.__create_random_delay()
         print(f"Publishing to {topic} with delay {delay}")
         await asyncio.sleep(delay)
         await self.nc.publish(topic, data)
@@ -44,19 +48,42 @@ class Processor:
         await self.subscribe(self.INPKT_SEC)
         await self.subscribe(self.INPKT_INSEC)
         print(f"Subscribed to {self.INPKT_SEC} and {self.INPKT_INSEC} topics")
-
         try:
             while True:
                 await asyncio.sleep(1)
+                self.__write_mean_delay()
         except KeyboardInterrupt:
             print("Disconnecting...")
             await self.nc.close()
 
-    def create_random_delay(self):
-        return random.expovariate(1 / 5e-6)
-
+    def __create_random_delay(self):
+        delay = random.expovariate(1 / (self.delay_mean * 1e-3))
+        self.__push_delay(delay * 1000)
+        return delay
     
+    def __push_delay(self, delay):
+        if self.delays.full():
+            self.delays.get()
+        self.delays.put(delay)
+
+    def __write_mean_delay(self):
+        if len(self.delays.queue) == 0:
+            return
+        average_delay = sum(self.delays.queue) / len(self.delays.queue)
+        print(f"{average_delay} with --delay-mean {self.delay_mean}\n")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Processor")
+    parser.add_argument(
+        "--delay-mean",
+        type=float,
+        default=5,
+        help="Mean delay in microseconds",
+    )
+    return parser.parse_args()
 
 if __name__ == '__main__':
-    processor = Processor()
+    args = parse_args()
+    processor = Processor(args.delay_mean)
     asyncio.run(processor.run())
